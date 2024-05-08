3cacf9f05add304077a502d1cb20ff6027af4629
henrique sydio <henriquesydio@hotmail.com>
2024-05-08 16:06:07 -0300

    Deploy

---
+++ Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Definição da classe Aluno
+public class Aluno
+{
+    // Atributos
+    private string alunoNome;
+    private int anoNasc;
+
+    // Propriedades
+    public string Nome
+    {
+        get { return alunoNome; }
+        set { alunoNome = value; }
+    }
+
+    public int Ano
+    {
+        get { return anoNasc; }
+        set { anoNasc = value; }
+    }
+
+    // Método
+    public void Estudar()
+    {
+        Console.WriteLine($"{Nome} está estudando.");
+    }
+
+    // Sobrecarga de Método
+    public void Estudar(string materia)
+    {
+        Console.WriteLine($"{Nome} está estudando {materia}.");
+    }
+}
+
+// Definição da classe Professor
+public class Professor
+{
+    // Atributos
+    private string professorNome;
+    private List<string> disciplinas = new List<string>();
+
+    // Propriedades
+    public string NomeProfessor
+    {
+        get { return professorNome; }
+        set { professorNome = value; }
+    }
+
+    // Método
+    public void Ensinando()
+    {
+        Console.WriteLine($"{NomeProfessor} está ensinando.");
+    }
+
+    // Sobrecarga de Método
+    public void Ensinando(string disciplina)
+    {
+        disciplinas.Add(disciplina);
+        Console.WriteLine($"{NomeProfessor} está ensinando {disciplina}.");
+    }
+}
+
+// Definição da classe InstituicaoEnsino com relacionamento unidirecional
+public class InstituicaoEnsino
+{
+    // Atributos
+    private List<Aluno> alunos = new List<Aluno>();
+
+    // Método para adicionar aluno
+    public void AdicionarAluno(Aluno aluno)
+    {
+        alunos.Add(aluno);
+        Console.WriteLine($"Aluno {aluno.Nome} adicionado à instituição de ensino.");
+    }
+}
+
+// Definição da classe Escola com relacionamento bidirecional
+public class Escola
+{
+    // Atributos
+    private List<Aluno> alunos = new List<Aluno>();
+    private List<Professor> professores = new List<Professor>();
+
+    // Método para adicionar aluno
+    public void AdicionarAluno(Aluno aluno)
+    {
+        alunos.Add(aluno);
+        aluno.Estudar(); // Exemplo de uso do método do aluno
+        Console.WriteLine($"Aluno {aluno.Nome} adicionado à escola.");
+    }
+
+    // Método para adicionar professor
+    public void AdicionarProfessor(Professor professor)
+    {
+        professores.Add(professor);
+        professor.Ensinando(); // Exemplo de uso do método do professor
+        Console.WriteLine($"Professor {professor.NomeProfessor} adicionado à escola.");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static Aluno;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        aluno1.Nome = "Leonel";
+        aluno1.Ano = 1980;
+
+        Aluno aluno2 = new Aluno();
+        aluno2.Nome = "Pedro";
+        aluno2.Ano = 2004;
+
+        Professor professor1 = new Professor();
+        professor1.NomeProfessor = "Thiaguinho";
+
+        Professor professor2 = new Professor();
+        professor2.NomeProfessor = "Rosen";
+
+        InstituicaoEnsino instituicao = new InstituicaoEnsino();
+
+        instituicao.AdicionarAluno(aluno1);
+        instituicao.AdicionarAluno(aluno2);
+
+        Escola escola = new Escola();
+
+        escola.AdicionarAluno(aluno1);
+        escola.AdicionarAluno(aluno2);
+        escola.AdicionarProfessor(professor1);
+        escola.AdicionarProfessor(professor2);
+
+        // Exemplo de sobrecarga de método
+        aluno1.Estudar("Progamação Mobile");
+        professor2.Ensinando("Orientação a Objetos");
+    }
+}
